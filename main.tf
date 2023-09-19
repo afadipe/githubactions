04e4ba3c40ed2be0b30720980926d9bf71e17650
Wasiu Ayobami Fadipe <wasiu.ayobami.fadipe@ericsson.com>
2023-09-19 10:57:34 +0200

    change ami data source

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2" {
-  ami           = data.aws_ami.linux_ami
+  ami           = data.aws_ami.linux_ami.id
   instance_type = var.instance-type
   tags = merge(
     var.default_tags,
