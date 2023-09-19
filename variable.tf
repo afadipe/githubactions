0bc230d9575841567d0258da1348e132212da8d5
Wasiu Ayobami Fadipe <wasiu.ayobami.fadipe@ericsson.com>
2023-09-19 13:32:45 +0200

    terraform -var-file

--- variable.tf
+++ variable.tf
@@ -1,19 +1,19 @@
 variable "instance-type" {
   type        = string
   description = "Instance Type"
-  default     = "t3.micro"
+  # default     = "t3.micro"
 }
 
 variable "region" {
   type        = string
   description = "AWS Region ID"
-  default     = "eu-north-1"
+  # default     = "eu-north-1"
 }
 
 variable "ec-name" {
   type        = string
   description = "Instance Name"
-  default     = "GitHub Action Terraform"
+  #default     = "GitHub Action Terraform"
 }
 
 
